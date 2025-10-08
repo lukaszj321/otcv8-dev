e3e715798683d217c06871f4a12a931901d7c78d
lukaszj321 <32397745+lukaszj321@users.noreply.github.com>
2025-10-08 07:01:51 +0200

    Delete scripts/mkdocs_modules_hook.py

--- scripts/mkdocs_modules_hook.py
+++
@@ -1,7 +0,0 @@
-# scripts/mkdocs_modules_hook.py
-from mkdocs.structure.files import Files
-
-def on_files(files, config):
-    """Zostaw w buildzie TYLKO pliki z docs/modules/**."""
-    kept = [f for f in files if f.src_uri.startswith("modules/")]
-    return Files(kept)
