0966c83a5e1c67cc36db7ee43b643f3988c69cb2
lukaszj321 <32397745+lukaszj321@users.noreply.github.com>
2025-10-08 06:49:47 +0200

    Update mkdocs_modules_hook.py

--- scripts/mkdocs_modules_hook.py
+++ scripts/mkdocs_modules_hook.py
@@ -1,10 +1,7 @@
 # scripts/mkdocs_modules_hook.py
-
 from mkdocs.structure.files import Files
 
 def on_files(files, config):
-    """
-    Zostaw w buildzie TYLKO pliki z katalogu docs/modules/**.
-    """
+    """Zostaw w buildzie TYLKO pliki z docs/modules/**."""
     kept = [f for f in files if f.src_uri.startswith("modules/")]
     return Files(kept)
